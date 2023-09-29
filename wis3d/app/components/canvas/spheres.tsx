fea6a7149e84bb0d9430a586492a5ae2c61a476c
ootts <clh.chenlinghao@gmail.com>
2023-09-28 19:12:57 -0700

    add gmat working.

--- wis3d/app/components/canvas/spheres.tsx
+++ wis3d/app/components/canvas/spheres.tsx
@@ -1,4 +1,4 @@
-import {memo, MutableRefObject, useRef} from 'react';
+import {memo, MutableRefObject, useLayoutEffect, useRef} from 'react';
 import {useXHR} from "@/utils/hooks";
 import {Color, Material, Side} from "three"
 import {centerOnDbClick} from "./trackball-controls";
@@ -11,6 +11,9 @@ interface IProps {
     sphereColors?: boolean;
 }
 
+function gmat() {
+    return (<meshNormalMaterial transparent={true} opacity={0.5}/>);
+}
 
 function getMaterial(
     material: string,
@@ -22,53 +25,40 @@ function getMaterial(
     // shininess: number,
     // side: Side
 ) {
-    console.log("material", material);
-    switch (material) {
-        // case "MeshStandardMaterial":
-        //   return (
-        //     <meshStandardMaterial
-        //       ref={ref}
-        //       vertexColors={vertexColors}
-        //       color={color}
-        //       wireframe={wireframe}
-        //       side={side}
-        //       flatShading={flatShading}
-        //     />
-        //   );
-        case "MeshNormalMaterial":
-            return (
-                <meshNormalMaterial
-                    ref={ref}
-                    // vertexColors={vertexColors}
-                    color={color}
-                    // wireframe={wireframe}
-                    // side={side}
-                    // flatShading={flatShading}
-                />
-            );
-        // case "MeshPhongMaterial":
-        //   return (
-        //     <meshPhongMaterial
-        //       ref={ref}
-        //       vertexColors={vertexColors}
-        //       color={color}
-        //       wireframe={wireframe}
-        //       side={side}
-        //       flatShading={flatShading}
-        //       shininess={shininess}
-        //     />
-        //   );
-        case "MeshBasicMaterial":
-        default:
-            return (
-                <meshBasicMaterial ref={ref}
-                    // vertexColors={vertexColors}
-                                   color={color}
-                    // wireframe={wireframe}
-                    // side={side}
-                />
-            );
-    }
+    return (
+        <meshNormalMaterial
+            ref={ref}
+            // vertexColors={vertexColors}
+            // color={color}
+            // wireframe={wireframe}
+            // side={side}
+            // flatShading={flatShading}
+        />
+    );
+    // console.log("material", material);
+    // switch (material) {
+    //     case "MeshNormalMaterial":
+    //         return (
+    //             <meshNormalMaterial
+    //                 ref={ref}
+    //                 // vertexColors={vertexColors}
+    //                 color={color}
+    //                 // wireframe={wireframe}
+    //                 // side={side}
+    //                 // flatShading={flatShading}
+    //             />
+    //         );
+    //     case "MeshBasicMaterial":
+    //     default:
+    //         return (
+    //             <meshBasicMaterial ref={ref}
+    //                 // vertexColors={vertexColors}
+    //                                color={color}
+    //                 // wireframe={wireframe}
+    //                 // side={side}
+    //             />
+    //         );
+    // }
 }
 
 export const Spheres = memo<IProps>(function Spheres(props) {
@@ -83,19 +73,19 @@ export const Spheres = memo<IProps>(function Spheres(props) {
                 const {center, radius, color, scales, quaternion} = sphere;
                 const userColor = color ? color.map(x => x / 250) : undefined;
                 // const materialRef = useRef<Material>();
+                // useLayoutEffect(() => {
+                //     if (materialRef.current) materialRef.current.needsUpdate = true;
+                // }, );
+
+
                 return (
                     <mesh position={center} quaternion={quaternion} scale={scales} key={index}
                           onDoubleClick={centerOnDbClick}>
-                        {/*{*/}
-                        {/*    getMaterial(material, materialRef,*/}
-                        {/*        sphereColors && userColor ? userColor : defaultColor)*/}
-                        {/*}*/}
+
                         <sphereBufferGeometry args={[radius, 30, 30]}/>
-                        {/*<meshBasicMaterial*/}
-                        {/*    color={sphereColors && userColor ? userColor : defaultColor}*/}
-                        {/*    transparent={true}*/}
-                        {/*    opacity={0.5}/>*/}
-                        <meshNormalMaterial transparent={true} opacity={0.5}/>
+
+                        {gmat()}
+
                     </mesh>
                 )
             })}
